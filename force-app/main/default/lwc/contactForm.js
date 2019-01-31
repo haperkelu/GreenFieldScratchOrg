428553635b79251bad992ab17b9f65267dfdb660
Pie <haperkelu@gmail.com>
2019-01-31 11:02:41 +1100

    Delete contactForm.js

--- force-app/main/default/lwc/contactForm.js
+++
@@ -1,8 +0,0 @@
-import { LightningElement, api, track } from 'lwc';
-
-export default class ContactForm extends LightningElement {
-
-    @api recordId;
-    @track fields = ['Name', 'Title', 'Phone', 'Email'];
- 
-}
